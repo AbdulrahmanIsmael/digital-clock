b58ac4c40404d6940fbc2b73360ae00bcb61b4f0
Abdulrahman Ismael <116180351+AbdulrahmanIsmael@users.noreply.github.com>
2025-08-08 01:52:51 +0300

    refactor: removed extra spaces in the code

--- main.c
+++ main.c
@@ -6,21 +6,15 @@
 int main()
 {
     time_t rawtime = 0;
-
     struct tm *pTime = NULL;
-
     bool isRunning = true;
 
     printf("*** Digital Clock ***\n\n");
-
     while (isRunning)
     {
         time(&rawtime);
-
         pTime = localtime(&rawtime);
-
         printf("\r%02d:%02d:%02d", pTime->tm_hour, pTime->tm_min, pTime->tm_sec);
-
         Sleep(1000);
     }
 }
